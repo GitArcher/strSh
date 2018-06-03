c9a8caa34200e6a6928990f10a6009af8b4636c2
GitArcher <32144915+GitArcher@users.noreply.github.com>
2018-06-03 20:21:58 +0300

    Add files via upload

--- serv.js
+++ serv.js
@@ -1,3 +1,12 @@
+const os = require('os');
+const intInfo = os.networkInterfaces();
+let ip;
+for (let devName in intInfo) {
+  if(devName !== 'lo') {
+    ip = intInfo[devName][0].address;
+  }
+}
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
@@ -5,4 +14,6 @@ const port = process.env.PORT || 8080;
 app.use('/res', express.static(__dirname+'/res'));
 app.get('/', (req, res) => { res.sendFile(__dirname+'/index.html') });
 
-app.listen(port,'192.168.1.100');
+app.listen(port, ip, () => {
+  console.log("http://"+ip+":"+port);
+});
